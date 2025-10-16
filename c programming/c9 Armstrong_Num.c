57b8375e01b314ba66fffa84575ee826b9dfebb1
Yamini-bhoksa <24B11DS027@adityauniversity.in>
2025-10-16 15:54:33 +0530

    Add files via upload

---
+++ c programming/c9 Armstrong_Num.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, temp, sum = 0, digits = 0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    temp = n;
+
+    while(temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+    while(temp != 0) {
+        rem = temp % 10;
+        sum += pow(rem, digits);
+        temp /= 10;
+    }
+
+    if(sum == n)
+        printf("%d is an Armstrong number\n", n);
+    else
+        printf("%d is not an Armstrong number\n", n);
+
+    return 0;
+}
